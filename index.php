ded823ff8188abd70fef1f5985d9c8af2993d7a3
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-30 11:55:43 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Explore Family Law with FamilyLawAct.xyz</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Explore Family Law with FamilyLawAct.xyz</h1>
+        <p>FamilyLawAct.xyz provides comprehensive information on various family law matters. From legal procedures to rights and responsibilities, we guide you through the complexities of family law. Whether you're dealing with divorce, custody, or more, we're here to help.</p>
+        <p>For more details, visit <a href="https://familylawact.xyz/">FamilyLawAct.xyz</a> or find information about <a href="https://familylawact.xyz/">Divorce</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a>
+
+    </div>
+</body>
+
+</html>
